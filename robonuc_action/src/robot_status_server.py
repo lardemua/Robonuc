45aa72daa6f383971ce672d180b9cb7a7219b685
TiagoaTavares <tiagoa.tavares@hotmail.com>
2019-04-08 17:29:33 +0100

    final_push_try

--- robonuc_action/src/robot_status_server.py
+++ robonuc_action/src/robot_status_server.py
@@ -3,51 +3,47 @@ import rospy
 
 import actionlib
 #changes
-import robonuc_action.msg
+import actionlib_tutorials.msg
 
 class FibonacciAction(object):
     # create messages that are used to publish feedback/result
-    _feedback = robonuc_action.msg.Robot_statusActionFeedback()
-    _result = robonuc_action.msg.Robot_statusActionResult()
+    _feedback = actionlib_tutorials.msg.FibonacciFeedback()
+    _result = actionlib_tutorials.msg.FibonacciResult()
 
     def __init__(self, name):
         self._action_name = name
-        self._as = actionlib.ActionServer(self._action_name, robonuc_action.msg.Robot_statusAction, self.execute_cb, self.internal_preempt_callback, auto_start = False)
-        self._as.start() #server on
+        self._as = actionlib.SimpleActionServer(self._action_name, actionlib_tutorials.msg.FibonacciAction, execute_cb=self.execute_cb, auto_start = False)
+        self._as.start()
       
-    def internal_preempt_callback(self):
-        pass
-
     def execute_cb(self, goal):
-
-        
-
-        # rospy.loginfo("Received GOAL " + str(goal.mode) )
         # helper variables
         r = rospy.Rate(1)
         success = True
         
-        # goal.set
-        # self._as.accept_new_goal();
-        # self._as.getState()
-        success=True
+        # append the seeds for the fibonacci sequence
+        self._feedback.sequence = []
+        self._feedback.sequence.append(0)
+        self._feedback.sequence.append(1)
         
-        # # start executing the action
-        # for i in range(1, goal.order):
-        #     # check that preempt has not been requested by the client
-        #     if self._as.is_preempt_requested():
-        #         rospy.loginfo('%s: Preempted' % self._action_name)
-        #         self._as.set_preempted()
-        #         success = False
-        #         break
-        #     self._feedback.sequence.append(self._feedback.sequence[i] + self._feedback.sequence[i-1])
-        #     # publish the feedback
-        #     self._as.publish_feedback(self._feedback)
-        #     # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
-        #     r.sleep()
+        # publish info to the console for the user
+        rospy.loginfo('%s: Executing, creating fibonacci sequence of order %i with seeds %i, %i' % (self._action_name, goal.order, self._feedback.sequence[0], self._feedback.sequence[1]))
+        
+        # start executing the action
+        for i in range(1, goal.order):
+            # check that preempt has not been requested by the client
+            if self._as.is_preempt_requested():
+                rospy.loginfo('%s: Preempted' % self._action_name)
+                self._as.set_preempted()
+                success = False
+                break
+            self._feedback.sequence.append(self._feedback.sequence[i] + self._feedback.sequence[i-1])
+            # publish the feedback
+            self._as.publish_feedback(self._feedback)
+            # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
+            r.sleep()
           
         if success:
-            # self._result.sequence = self._feedback.sequence
+            self._result.sequence = self._feedback.sequence
             rospy.loginfo('%s: Succeeded' % self._action_name)
             self._as.set_succeeded(self._result)
         
